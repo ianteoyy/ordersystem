24bf79b05a61d0960690d6e3057b3b6ca53e7ebc
Ian <fishy90@gmail.com>
2020-08-04 16:33:38 +0800

    add env file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -7,6 +8,7 @@ import { OrdersModule } from './orders/orders.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     OrdersModule,
     MongooseModule.forRoot(
       `mongodb+srv://test2:${process.env.MONGODB_PASSWORD}@orders.dxs0h.mongodb.net/order-system?retryWrites=true&w=majority`,
